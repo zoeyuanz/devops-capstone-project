2958c4a42b75a6147e7fa1cb1835c9061e5c82a9
zoeyuanz <zoeyuan203@gmail.com>
2024-10-11 03:48:51 -0400

    add GET endpoint for listing all accounts

--- service/routes.py
+++ service/routes.py
@@ -60,8 +60,15 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST all the accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
